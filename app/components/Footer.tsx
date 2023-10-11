28f01a3dbb9ade98265c1502ec70ed1266e057c4
kelvin muthama <kelvinkamau387@gmail.com>
2023-10-11 22:04:16 +0300

    Style: Add style for footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,33 @@
 
 const Footer = () => {
   return <div>Footer</div>;
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12">
+        <div className="flex items-start">
+          <Image
+            src="/logo-mustard.svg"
+            width={115}
+            height={38}
+            alt="Design Hive"
+          />
+
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Design Hive: Where Designers Flourish. Cultivating Creativity,
+            Sharing Vision, and Nurturing Artistry in a Community of Infinite
+            Inspiration.
+          </p>
+        </div>
+
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
