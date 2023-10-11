4883dcbfd8d68b6496221657fa22465333388964
kelvin muthama <kelvinkamau387@gmail.com>
2023-10-11 22:05:17 +0300

    Feat: Create props for column

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,14 @@
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+  <div className="footer_column">
+    <h4 className="font-semibold">Title</h4>
+    <ul className="flex flex-col gap-2 font-normal">Links</ul>
+  </div>
+);
 
 const Footer = () => {
   return <div>Footer</div>;
